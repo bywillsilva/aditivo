3f087b55e7a4b3908810327b035287c930caeb3e
bywillsilva <raniwillere@gmail.com>
2024-11-18 10:04:14 -0300

    updating news

--- assets/js/news/news.js
+++ assets/js/news/news.js
@@ -18,7 +18,13 @@ let divs = [];
 let url = 'https://newsapi.org/v2/everything?q=contábil&apiKey=55328aac8b954a94b71a265545300c65';
 
 const getNoticia = () => {
-    fetch(url)
+    fetch(url, 
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
         .then((res) => {
             return res.json()
         }).then((res) => {
